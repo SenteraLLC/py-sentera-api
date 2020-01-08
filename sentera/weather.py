d0c51e4625ac3d67203a9e4d41ef480c727a7a71
Elliott Imhoff <elliott.imhoff@sentera.com>
2020-01-08 11:10:22 -0600

    Added more support for different weather types

--- sentera/weather.py
+++ sentera/weather.py
@@ -8,8 +8,8 @@ WEATHER_HEADER = {'X-API-Key': 'mc049Cu9FJ3lHiQYDYQTd3ZOzsOBt29d2gyi3e0r'}
 class WeatherType(Enum):
     Recent = "recent"
     Historical = "historical"
-    Forecast = "forecast"
-    Current = "current"
+    # Forecast = "forecast"
+    # Current = "current"
 
     def __str__(self):
         return str(self.value)
@@ -22,11 +22,9 @@ class WeatherType(Enum):
 class WeatherVariable(Enum):
     Temperature = "temperature"
     Humidity = "relative-humidity"
-    # DewPoint = "DewPoint"
-    # WindSpeed = "WindSpeed"
-    # WindDirection = "WindDirection"
-    # Precipitation = "Precipitation"
-    # SolarRadiation = "SolarRadiation"
+    HighTemperature = "high-temperature"
+    LowTemperature = "low-temperature"
+    Precipitation = "precipitation"
 
     def __str__(self):
         return str(self.value)
@@ -48,22 +46,53 @@ class WeatherInterval(Enum):
         return value in cls._value2member_map_
 
 
-def build_weather_url(type, variable, interval, lat, long):
-    return posixpath.join(WEATHER_BASE_URL, str(type), f"{interval}-{variable}", str(lat), str(long))
+TIME_COLUMNS = {WeatherInterval.Hourly: "validTime", WeatherInterval.Daily: "validDate"}
+PARAMETER_COMBINATIONS = {
+                            WeatherType.Recent:
+                            {
+                                WeatherInterval.Hourly:
+                                    [WeatherVariable.Temperature,
+                                     WeatherVariable.Humidity],
+                                WeatherInterval.Daily:
+                                    [WeatherVariable.HighTemperature,
+                                     WeatherVariable.LowTemperature,
+                                     WeatherVariable.Precipitation]
+                            },
+                            WeatherType.Historical:
+                            {
+                                WeatherInterval.Daily:
+                                    [WeatherVariable.HighTemperature,
+                                     WeatherVariable.LowTemperature,
+                                     WeatherVariable.Precipitation]
+                            }
+                         }
 
 
-def create_params(weather_variable, time_interval):
-    if weather_variable == WeatherVariable.Temperature:
-        return {
-            'start': time_interval[0],
-            'end': time_interval[1],
-            'unit': 'fahrenheit'
-        }
+def build_weather_url(weather_type, weather_variable, weather_interval, lat, long):
+    try:
+        if weather_variable not in PARAMETER_COMBINATIONS[weather_type][weather_interval]:
+            raise KeyError
+    except KeyError:
+        raise ValueError(f"Parameter combination not allowed: {weather_type}, {weather_variable}, {weather_interval}")
 
-    if weather_variable == WeatherVariable.Humidity:
-        return {
-            'start': time_interval[0],
-            'end': time_interval[1]
-        }
+    return posixpath.join(WEATHER_BASE_URL,
+                          str(weather_type),
+                          f"{weather_interval}-{weather_variable}",
+                          str(lat),
+                          str(long))
 
-    raise ValueError(f"Weather variable not supported: {weather_variable}")
+
+def create_params(weather_type, time_interval):
+    param_dict = {}
+
+    if weather_type == WeatherType.Recent:
+        param_dict['start'] = time_interval[0]
+        param_dict['end'] = time_interval[1]
+
+    if weather_type == WeatherType.Historical:
+        param_dict['start'] = "01-01"
+        param_dict['end'] = "12-31"
+        param_dict['years'] = 10
+
+    return param_dict
+
