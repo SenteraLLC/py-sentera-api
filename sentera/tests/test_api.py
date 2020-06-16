7f3eafc95980b59cafb68aae39bd926dfa1c37b0
ZachMarston <zach.p.marston@gmail.com>
2020-06-16 15:17:26 -0500

    changed tests to include url

--- sentera/tests/test_api.py
+++ sentera/tests/test_api.py
@@ -10,8 +10,8 @@ from ..api import create_alert, get_weather
 @httpretty.httprettified
 def test_create_alert_with_key_success():
     def request_callback(request, uri, response_headers):
-        mutation = b"mutation CreateAlert ($field_sentera_id: ID!, $name: String!, $message: String!, $key: String) {\\n    create_alert ("
-        variables = b'"variables": {"field_sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003", "name": "Southern Corn Rust Alert", "message": "Current weather conditions indicate things.", "key": "corn_rust"}'
+        mutation = b"mutation CreateAlert ($field_sentera_id: ID!, $name: String!, $message: String!, $url: String, $key: String) {\\n    create_alert ("
+        variables = b'"variables": {"field_sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003", "name": "Southern Corn Rust Alert", "message": "Current weather conditions indicate things.", "url": "https://www.sentera.com", "key": "corn_rust"}'
         assert variables in request.body
         assert mutation in request.body
         return [
@@ -23,6 +23,7 @@ def test_create_alert_with_key_success():
                         "create_alert": {
                             "sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003",
                             "key": "corn_rust",
+                            "url": "https://www.sentera.com",
                             "name": "Southern Corn Rust Alert",
                             "message": "Current weather conditions indicate things.",
                             "created_at": "2020-03-26T18:20:03Z",
@@ -40,6 +41,7 @@ def test_create_alert_with_key_success():
         name="Southern Corn Rust Alert",
         message="Current weather conditions indicate things.",
         token="token123",
+        url="https://www.sentera.com",
         key="corn_rust",
     )
     assert response["data"]["create_alert"]["key"] == "corn_rust"
@@ -49,8 +51,8 @@ def test_create_alert_with_key_success():
 @httpretty.httprettified
 def test_create_alert_without_key_success():
     def request_callback(request, uri, response_headers):
-        mutation = b"mutation CreateAlert ($field_sentera_id: ID!, $name: String!, $message: String!, $key: String) {\\n    create_alert ("
-        variables = b'"variables": {"field_sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003", "name": "Southern Corn Rust Alert", "message": "Current weather conditions indicate things.", "key": null}'
+        mutation = b"mutation CreateAlert ($field_sentera_id: ID!, $name: String!, $message: String!, $url: String, $key: String) {\\n    create_alert ("
+        variables = b'"variables": {"field_sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003", "name": "Southern Corn Rust Alert", "message": "Current weather conditions indicate things.", "url": null, "key": null}'
         assert variables in request.body
         assert mutation in request.body
         return [
@@ -62,6 +64,7 @@ def test_create_alert_without_key_success():
                         "create_alert": {
                             "sentera_id": "gyq8ll6_AL_8brhbkSentera_CV_shar_e599fde_200326_182003",
                             "key": None,
+                            "url": None,
                             "name": "Southern Corn Rust Alert",
                             "message": "Current weather conditions indicate things.",
                             "created_at": "2020-03-26T18:20:03Z",
